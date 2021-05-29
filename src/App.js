16854aa218e5b764634dffa5dbe6236f10c82700
Denys Filonenko <denisfilonenko@meta.ua>
2021-05-29 14:43:35 +0300

    added new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Info from './components/info';
+import Form from './components/form';
+import Weather from './components/weather';
+
+const API_KEY = '7992188bc2460cf5f56bf8d34431fd37';
 
 class App extends React.Component {
+
+  gettingWeather = async (e) => {
+    e.preventDefault();
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
         <Info />
+        <Form weatherMethod={this.gettingWeather} />
+        <Weather />
       </div>
     );
   }
