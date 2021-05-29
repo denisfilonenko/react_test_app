9bd8457323b3c10070b07c6ddb904626d779b1db
Denys Filonenko <denisfilonenko@meta.ua>
2021-05-29 10:46:17 +0300

    start project

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Weather application</h2>
+                <p>Find out the weather in your city</p>
+            </div>
+        )
+    }
+}
+
+export default Info;
