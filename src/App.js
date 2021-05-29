31e51e5596d0fe68f28b5286da1dc35256fc0cae
Denys Filonenko <denisfilonenko@meta.ua>
2021-05-29 16:47:01 +0300

    app done

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ const API_KEY = '7992188bc2460cf5f56bf8d34431fd37';
 class App extends React.Component {
 
   state = {
+    description: undefined,
     temp: undefined,
     city: undefined,
     country: undefined,
     pressure: undefined,
     sunset: undefined,
+    windspeed: undefined,
     error: undefined
   }
   
@@ -24,26 +26,44 @@ class App extends React.Component {
       const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await api_url.json();
 
+      if (data.cod === "404") {
+        this.setState({
+          description: undefined,
+          temp: undefined,
+          city: undefined,
+          country: undefined,
+          pressure: undefined,
+          sunset: undefined,
+          windspeed: undefined,
+          error: "Enter valid name of the city"
+        })
+        return
+      }
+
       var sunset = data.sys.sunset;
       var date = new Date();
       date.setTime(sunset);
       var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
       this.setState({
+        description: data.weather[0].description,
         temp: data.main.temp,
         city: data.name,
         country: data.sys.country,
         pressure: data.main.pressure,
         sunset: sunset_date,
+        windspeed: data.wind.speed,
         error: undefined
       })
     } else {
       this.setState({
+        description: undefined,
         temp: undefined,
         city: undefined,
         country: undefined,
         pressure: undefined,
         sunset: undefined,
+        windspeed: undefined,
         error: "Enter the name of the city"
       })
     }
@@ -52,17 +72,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Info />
-        <Form weatherMethod={this.gettingWeather} />
-        <Weather 
-          temp={this.state.temp}
-          city={this.state.city}
-          country={this.state.country}
-          pressure={this.state.pressure}
-          sunset={this.state.sunset}
-          error={this.state.error}
-        />
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-5 info">
+                <Info />
+              </div>
+              <div className="col-sm-7 form">
+                <Form weatherMethod={this.gettingWeather} />
+                <Weather 
+                  description={this.state.description}
+                  temp={this.state.temp}
+                  city={this.state.city}
+                  country={this.state.country}
+                  pressure={this.state.pressure}
+                  sunset={this.state.sunset}
+                  windspeed={this.state.windspeed}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const Weather = props => (
-    <div>
+    <div className="infoWeath">
     { props.city && 
         <>
-            <p>Location: {props.city}, {props.country}</p>
-            <p>Temperature: {props.temp}</p>
-            <p>Pressure: {props.pressure}</p>
-            <p>Sunset: {props.sunset}</p>
+            <p><span>Location:</span> {props.city}, {props.country}</p>
+            <p><span>Weather:</span> {props.description}</p>
+            <p><span>Temperature:</span> {props.temp}&deg;</p>
+            <p><span>Pressure:</span> {props.pressure} mm</p>
+            <p><span>Sunset:</span> {props.sunset}</p>
+            <p><span>Wind speed:</span> {props.windspeed} m/s</p>
         </>
     }
-        <p>{ props.error }</p>
+        <p className="error">{ props.error }</p>
     </div>
 );
 
