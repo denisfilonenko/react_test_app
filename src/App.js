1abb1d752940128c8f1aeb6f7a66516ec4ec2282
Denys Filonenko <denisfilonenko@meta.ua>
2021-05-29 15:05:18 +0300

    getting data from api

--- src/App.js
+++ src/App.js
@@ -7,11 +7,30 @@ const API_KEY = '7992188bc2460cf5f56bf8d34431fd37';
 
 class App extends React.Component {
 
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+  
   gettingWeather = async (e) => {
     e.preventDefault();
-    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value;
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_url.json();
     console.log(data);
+
+    this.setState({
+      temp: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      sunrise: data.sys.sunrise,
+      sunset: data.sys.sunset,
+      error: ""
+    })
   }
 
   render() {
@@ -19,7 +38,14 @@ class App extends React.Component {
       <div>
         <Info />
         <Form weatherMethod={this.gettingWeather} />
-        <Weather />
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -5,6 +5,10 @@ class Weather extends React.Component {
         return (
             <div>
                 
+                <p>Location: {this.props.city}, {this.props.country}</p>
+                <p>Temperature: {this.props.temp}</p>
+                <p>Sunrise: {this.props.sunrise}</p>
+                <p>Sunset: {this.props.sunset}</p>
             </div>
         )
     }
